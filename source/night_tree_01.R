047a65255316d6746579d1b266d8cb6969f5f15c
Danielle Navarro <d.navarro@unsw.edu.au>
2021-01-24 07:06:15 +1100

    allows vector of seeds

--- source/night_tree_01.R
+++ source/night_tree_01.R
@@ -10,7 +10,7 @@ library(ggforce)
 
 # constants ---------------------------------------------------------------
 
-seed <- 105
+seeds <- 131:150
 system_id <- "01"
 system_name <- "night_tree"
 background <- "#32383D"
@@ -32,15 +32,10 @@ generate_shades <- function(n, seed) {
 
 generate_nebula <- function(n, seed) {
   set.seed(seed)
-  nebula_data <- nebula(iterations = n) %>%
-    as_tibble(.name_repair = "universal") %>%
-    rename(
-      x0 = ...1,
-      y0 = ...2,
-      x1 = ...3,
-      y1 = ...4,
-      shade = ...5
-    ) %>%
+  nebula_data <- nebula(iterations = n)
+  colnames(nebula_data) <- c("x0", "y0", "x1", "y1", "shade")
+  nebula_data <- nebula_data %>%
+    as_tibble() %>%
     slice(-(1:100))
   return(nebula_data)
 }
@@ -60,80 +55,83 @@ generate_tree <- function(n, seed) {
 }
 
 
-# create data etc ---------------------------------------------------------
+for(seed in seeds) {
+  cat(seed, "\n")
 
-nebula_shades <- generate_shades(8, seed)
-nebula_data <- generate_nebula(8000000, seed)
-tree_data <- generate_tree(14, seed)
-leaf_data <- tree_data %>% filter(id_time >= 13)
+  # create data etc ---------------------------------------------------------
 
-
-
-# create ggplot object ----------------------------------------------------
-
-pic <- ggplot() +
-  geom_segment(
-    data = nebula_data,  # draw nebula first
-    mapping = aes(
-      x = x0,
-      y = y0,
-      xend = x1,
-      yend = y1,
-      colour = shade
-    ),
-    show.legend = FALSE,
-    size = .1,
-    alpha = .2
-  ) +
-  geom_bezier(
-    data = tree_data,  # draw tree trunk next
-    mapping = aes(
-      x = coord_x,
-      y = coord_y,
-      size = seg_wid * 6,
-      group = id_path
-    ),
-    colour = tree_shade,
-    show.legend = FALSE,
-    lineend = "round",
-    alpha = 1
-  ) +
-  geom_point(
-    data = leaf_data,  # add leaves last
-    mapping = aes(
-      x = coord_x,
-      y = coord_y
-    ),
-    size = 1,
-    colour = leaf_shade,
-    stroke = 0,
-    show.legend = FALSE
-  ) +
-  theme_void() +
-  theme(panel.background = element_rect(
-    fill = background,
-    colour = background
-  )) +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_color_gradientn(colours = nebula_shades) +
-  scale_size_identity() +
-  coord_cartesian(xlim = c(.1, 1), ylim = c(.1, 1)) +
-  NULL
+  nebula_shades <- generate_shades(8, seed)
+  nebula_data <- generate_nebula(8000000, seed)
+  tree_data <- generate_tree(14, seed)
+  leaf_data <- tree_data %>% filter(id_time >= 13)
 
 
 
-# render the image --------------------------------------------------------
+  # create ggplot object ----------------------------------------------------
 
-output_file <- paste0(system_name, "_", system_id, "_", seed, ".jpg")
+  pic <- ggplot() +
+    geom_segment(
+      data = nebula_data,  # draw nebula first
+      mapping = aes(
+        x = x0,
+        y = y0,
+        xend = x1,
+        yend = y1,
+        colour = shade
+      ),
+      show.legend = FALSE,
+      size = .1,
+      alpha = .2
+    ) +
+    geom_bezier(
+      data = tree_data,  # draw tree trunk next
+      mapping = aes(
+        x = coord_x,
+        y = coord_y,
+        size = seg_wid * 6,
+        group = id_path
+      ),
+      colour = tree_shade,
+      show.legend = FALSE,
+      lineend = "round",
+      alpha = 1
+    ) +
+    geom_point(
+      data = leaf_data,  # add leaves last
+      mapping = aes(
+        x = coord_x,
+        y = coord_y
+      ),
+      size = 1,
+      colour = leaf_shade,
+      stroke = 0,
+      show.legend = FALSE
+    ) +
+    theme_void() +
+    theme(panel.background = element_rect(
+      fill = background,
+      colour = background
+    )) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_color_gradientn(colours = nebula_shades) +
+    scale_size_identity() +
+    coord_cartesian(xlim = c(.1, 1), ylim = c(.1, 1)) +
+    NULL
 
-ggsave(
-  plot = pic,
-  filename = output_file,
-  path = here("image"),
-  width = resolution/dpi,
-  height = resolution/dpi,
-  dpi = dpi
-)
 
 
+  # render the image --------------------------------------------------------
+
+  output_file <- paste0(system_name, "_", system_id, "_", seed, ".jpg")
+
+  ggsave(
+    plot = pic,
+    filename = output_file,
+    path = here("image"),
+    width = resolution/dpi,
+    height = resolution/dpi,
+    dpi = dpi
+  )
+
+}
